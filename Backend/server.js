7154f35cf0c4d86daf358e68406aa6078c834a43
Raj <75741866+rajdama@users.noreply.github.com>
2022-11-18 23:18:09 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const env = require('dotenv')
+const app = express()
+const cors = require('cors')
+
+env.config()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(`mongodb+srv://root:${process.env.MONGO_DB_PASSWORD}@cluster0.uf9usou.mongodb.net/?retryWrites=true&w=majority`,
+{
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+}
+    ).then(()=>{
+        console.log('database connected')
+            });
+
+const userRoutes = require('./routes/user')
+app.use('/api',userRoutes)
+
+app.listen(1000, () => {
+    console.log('Server is running on port 3000')
+})
