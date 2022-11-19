f18c7b5ac53d8ce88b3753e3dd246501e6a2eb11
Raj <75741866+rajdama@users.noreply.github.com>
2022-11-19 15:26:18 +0530

    Delete Backend directory

--- Backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { signup,signin } = require('../controller/user')
-const { validateSignupRequest,validateSigninRequest,isRequestValidated } = require('../validator/validator.js')
-const router = express.Router()
-
-router.post('/signin',validateSigninRequest,isRequestValidated,signin)
-router.post('/signup',validateSignupRequest,isRequestValidated,signup)
-
-module.exports = router
